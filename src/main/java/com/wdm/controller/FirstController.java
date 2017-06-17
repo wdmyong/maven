7efd42ed1c16f05f1a807d61b132dff05fa09b1f
yongduan <yongduan@sohu-inc.com>
2017-06-17 11:55:16 +0800

    first commit

---
+++ src/main/java/com/wdm/controller/FirstController.java
@@ -0,0 +1,12 @@
+package com.wdm.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
+import org.springframework.web.servlet.mvc.method.AbstractHandlerMethodAdapter;
+
+@RestController
+@RequestMapping("/first")
+public class FirstController {
+
+}
